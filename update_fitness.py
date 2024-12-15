9eb1f51159b50788c623e6f98f21e3ad556a9b48
lipe <lipebuiles@gmail.com>
2024-12-15 14:19:33 -0500

    feat: update fitness and in training 0, delete data related

--- update_fitness.py
+++ update_fitness.py
@@ -52,188 +52,238 @@ def update_fitness(id_health):
 
     id_user_update = int(get_logged_in_user_id())
     update_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    
-    query = "SELECT * FROM training WHERE id_health = %s"
-    values = (id_health,)
-    cursor.execute(query, values)
-    data = cursor.fetchall()
 
-    id_training = data[0][0]
-    id_type_training = data[0][2]
-    km_distance = data[0][3]
-    kcal_active = data[0][4]
-    kcal_total = data[0][5]
-    pace = str(data[0][6])
-    steps = data[0][7]
-    heart_rate_AVG = data[0][8]
 
-    new_id_type_training = input(f"El entrenamiento actual es {id_type_training}. Ingrese el nuevo entrenamiento o enter para conservar el actual: ")
-    if new_id_type_training == "":
-        new_id_type_training = int(id_type_training)
-    else:
-        new_id_type_training = int(new_id_type_training)
+    if new_in_training == '0':
+        query = "DELETE FROM dream WHERE id_health = %s"
+        values = (id_health,)
+        cursor.execute(query, values)
+        conn.commit()
 
-    new_km_distance = input(f"La distancia en km actual es {km_distance}. Ingrese la nueva distancia en km o enter para conservar la actual: ")
-    if new_km_distance == "":
-        new_km_distance = km_distance
+        query = "SELECT * FROM training WHERE id_health = %s"
+        values = (id_health,)
+        cursor.execute(query, values)
+        data = cursor.fetchall()
+
+        id_training = data[0][0]
+
+        query = "DELETE FROM cadence WHERE id_training = %s"
+        values = (id_training,)
+        cursor.execute(query, values)
+        conn.commit()
+
+        query = "DELETE FROM heart_rate WHERE id_training = %s"
+        values = (id_training,)
+        cursor.execute(query, values)
+        conn.commit()
+
+        query = "DELETE FROM pace WHERE id_training = %s"
+        values = (id_training,)
+        cursor.execute(query, values)
+        conn.commit()
+
+        query = "DELETE FROM pace_for_km WHERE id_training = %s"
+        values = (id_training,)
+        cursor.execute(query, values)
+        conn.commit()
+
+        query = "DELETE FROM stride_cm WHERE id_training = %s"
+        values = (id_training,)
+        cursor.execute(query, values)
+        conn.commit()
+
+        query = "DELETE FROM training WHERE id_health = %s"
+        values = (id_health,)
+        cursor.execute(query, values)
+        conn.commit()
+
+        query = "UPDATE health SET date = %s, calories = %s, steps = %s, distance = %s, moviment = %s, in_training = %s, id_user_update = %s, update_date = %s WHERE id = %s"
+        values = (new_date, new_calories, new_steps, new_distance, new_moviment, new_in_training, id_user_update, update_date, id_health)
+        cursor.execute(query, values)
+        conn.commit()
+
+    elif new_in_training == '1':  
+        query = "SELECT * FROM training WHERE id_health = %s"
+        values = (id_health,)
+        cursor.execute(query, values)
+        data = cursor.fetchall()
+
+        id_training = data[0][0]
+        id_type_training = data[0][2]
+        km_distance = data[0][3]
+        kcal_active = data[0][4]
+        kcal_total = data[0][5]
+        pace = str(data[0][6])
+        steps = data[0][7]
+        heart_rate_AVG = data[0][8]
+
+        new_id_type_training = input(f"El entrenamiento actual es {id_type_training}. Ingrese el nuevo entrenamiento o enter para conservar el actual: ")
+        if new_id_type_training == "":
+            new_id_type_training = int(id_type_training)
+        else:
+            new_id_type_training = int(new_id_type_training)
+
+        new_km_distance = input(f"La distancia en km actual es {km_distance}. Ingrese la nueva distancia en km o enter para conservar la actual: ")
+        if new_km_distance == "":
+            new_km_distance = km_distance
+            
+        new_kcal_active = input(f"Las calorias activas actuales son {kcal_active}. Ingrese las nuevas calorias activas o enter para conservar las actuales: ")
+        if new_kcal_active == "":
+            new_kcal_active = kcal_active
+
+        new_kcal_total = input(f"Las calorias totales actuales son {kcal_total}. Ingrese las nuevas calorias totales o enter para conservar las actuales: ")
+        if new_kcal_total == "":
+            new_kcal_total = kcal_total
+
+        new_pace = input(f"El ritmo actual es {pace}. Ingrese el nuevo ritmo o enter para conservar el actual: ")
+        if new_pace == "":
+            new_pace = pace
         
-    new_kcal_active = input(f"Las calorias activas actuales son {kcal_active}. Ingrese las nuevas calorias activas o enter para conservar las actuales: ")
-    if new_kcal_active == "":
-        new_kcal_active = kcal_active
+        new_steps = input(f"Los pasos actuales son {steps}. Ingrese los nuevos pasos o enter para conservar los actuales: ")
+        if new_steps == "":
+            new_steps = steps
+            
+        new_heart_rate_AVG = input(f"La frecuencia cardiaca promedio actual es {heart_rate_AVG}. Ingrese la nueva frecuencia cardiaca promedio o enter para conservar la actual: ")
+        if new_heart_rate_AVG == "":
+            new_heart_rate_AVG = heart_rate_AVG
 
-    new_kcal_total = input(f"Las calorias totales actuales son {kcal_total}. Ingrese las nuevas calorias totales o enter para conservar las actuales: ")
-    if new_kcal_total == "":
-        new_kcal_total = kcal_total
+        query = "SELECT * FROM cadence WHERE id_training = %s"
+        values = (id_training,)
+        cursor.execute(query, values)
+        data = cursor.fetchall()
 
-    new_pace = input(f"El ritmo actual es {pace}. Ingrese el nuevo ritmo o enter para conservar el actual: ")
-    if new_pace == "":
-        new_pace = pace
-    
-    new_steps = input(f"Los pasos actuales son {steps}. Ingrese los nuevos pasos o enter para conservar los actuales: ")
-    if new_steps == "":
-        new_steps = steps
+        cadence_AVG = data[0][2]
+        cadence_max = data[0][3]
+
+        new_cadence_AVG = input(f"La cadencia promedio actual es {cadence_AVG}. Ingrese la nueva cadencia promedio o enter para conservar la actual: ")
+        if new_cadence_AVG == "":
+            new_cadence_AVG = cadence_AVG
+
+        new_cadence_max = input(f"La cadencia máxima actual es {cadence_max}. Ingrese la nueva cadencia máxima o enter para conservar la actual: ")
+        if new_cadence_max == "":
+            new_cadence_max = cadence_max
+
+        query = "SELECT * FROM heart_rate WHERE id_training = %s"
+        values = (id_training,)
+        cursor.execute(query, values)
+        data = cursor.fetchall()
+
+        heart_rate_AVG = data[0][2]
+        heart_rate_max = data[0][3]
+        ligth_pace = data[0][4]
+        intensive_pace = data[0][5]
+        aerobic_pace = data[0][6]
+        anaerobic_pace = data[0][7]
+        vo2_max = data[0][8]
+
+        new_heart_rate_AVG = input(f"La frecuencia cardiaca promedio actual es {heart_rate_AVG}. Ingrese la nueva frecuencia cardiaca promedio o enter para conservar la actual: ")
+        if new_heart_rate_AVG == "":
+            new_heart_rate_AVG = heart_rate_AVG
+
+        new_heart_rate_max = input(f"La frecuencia cardiaca máxima actual es {heart_rate_max}. Ingrese la nueva frecuencia cardiaca máxima o enter para conservar la actual: ")
+        if new_heart_rate_max == "":
+            new_heart_rate_max = heart_rate_max
+            
+        new_ligth_pace = input(f"El ritmo ligero actual es {ligth_pace}. Ingrese el nuevo ritmo ligero o enter para conservar el actual: ")
+        if new_ligth_pace == "":
+            new_ligth_pace = ligth_pace
+
+        new_intensive_pace = input(f"El ritmo intenso actual es {intensive_pace}. Ingrese el nuevo ritmo intenso o enter para conservar el actual: ")
+        if new_intensive_pace == "":
+            new_intensive_pace = intensive_pace
+
+        new_aerobic_pace = input(f"El ritmo aeróbico actual es {aerobic_pace}. Ingrese el nuevo ritmo aeróbico o enter para conservar el actual: ")
+        if new_aerobic_pace == "":
+            new_aerobic_pace = aerobic_pace
+
+        new_anaerobic_pace = input(f"El ritmo anaeróbico actual es {anaerobic_pace}. Ingrese el nuevo ritmo anaeróbico o enter para conservar el actual: ")
+        if new_anaerobic_pace == "":
+            new_anaerobic_pace = anaerobic_pace
+
+        new_vo2_max = input(f"El VO2 máx actual es {vo2_max}. Ingrese el nuevo VO2 máx o enter para conservar el actual: ")
+        if new_vo2_max == "":
+            new_vo2_max = vo2_max
+
+        query = "SELECT * FROM pace WHERE id_training = %s"
+        values = (id_training,)
+        cursor.execute(query, values)
+        data = cursor.fetchall()
+
+        pace = data[0][2]
+        pace_max = data[0][3]
+
+        new_pace = input(f"El ritmo actual es {pace}. Ingrese el nuevo ritmo o enter para conservar el actual: ")
+        if new_pace == "":
+            new_pace = pace
+
+        new_pace_max = input(f"El ritmo máximo actual es {pace_max}. Ingrese el nuevo ritmo máximo o enter para conservar el actual: ")
+        if new_pace_max == "":
+            new_pace_max = pace_max
+
+        query = "SELECT * FROM stride_cm WHERE id_training = %s"
+        values = (id_training,)
+        cursor.execute(query, values)
+        data = cursor.fetchall()
+
+        stride_AVG = data[0][2]
+        stride_max = data[0][3]
+
+        new_stride_AVG = input(f"La zancada promedio actual es {stride_AVG}. Ingrese la nueva zancada promedio o enter para conservar la actual: ")
+        if new_stride_AVG == "":
+            new_stride_AVG = stride_AVG
+
+        new_stride_max = input(f"La zancada máxima actual es {stride_max}. Ingrese la nueva zancada máxima o enter para conservar la actual: ")
+        if new_stride_max == "":
+            new_stride_max = stride_max
+
+        query = "SELECT * FROM pace_for_km WHERE id_training = %s"
+        values = (id_training,)
+        cursor.execute(query, values)
+        data_km = cursor.fetchall()
+
+        query = "UPDATE health SET date = %s, calories = %s, steps = %s, distance = %s, moviment = %s, in_training = %s, id_user_update = %s, update_date = %s WHERE id = %s"
+        values = (new_date, new_calories, new_steps, new_distance, new_moviment, new_in_training, id_user_update, update_date, id_health)
+        cursor.execute(query, values)
+        conn.commit()
+
+        query = "UPDATE training SET id_type_training = %s, km_distance = %s, kcal_active = %s, kcal_total = %s, pace = %s, steps = %s, heart_rate_AVG = %s WHERE id_health = %s"
+        values = (new_id_type_training, new_km_distance, new_kcal_active, new_kcal_total, new_pace, new_steps, new_heart_rate_AVG, id_health)
+        cursor.execute(query, values)
+        conn.commit()
+
+        query = "UPDATE cadence SET cadence_AVG = %s, cadence_max = %s WHERE id_training = %s"
+        values = (new_cadence_AVG, new_cadence_max, id_training)
+        cursor.execute(query, values)
+        conn.commit()
+
+        query = "UPDATE heart_rate SET heart_rate_AVG = %s, heart_rate_max = %s, ligth_pace = %s, intensive_pace = %s, aerobic_pace = %s, anaerobic_pace = %s, vo2_max = %s WHERE id_training = %s"
+        values = (new_heart_rate_AVG, new_heart_rate_max, new_ligth_pace, new_intensive_pace, new_aerobic_pace, new_anaerobic_pace, new_vo2_max, id_training)
+        cursor.execute(query, values)
+        conn.commit()
+
+        query = "UPDATE pace SET pace = %s, pace_max = %s WHERE id_training = %s"
+        values = (new_pace, new_pace_max, id_training)
+        cursor.execute(query, values)
+        conn.commit()
+
+        query = "UPDATE stride_cm SET stride_AVG = %s, stride_max = %s WHERE id_training = %s"
+        values = (new_stride_AVG, new_stride_max, id_training)
+        cursor.execute(query, values)
+        conn.commit()
+
+        for i in data_km:
+                km = i[2]
+                pace = i[3]
+
+                new_pace = input(f"\nEl ritmo en el kilometro {km} fue {pace}. Ingrese el nuevo ritmo(HH:mm:ss) o enter para conservar el actual: ")
+                if new_pace == "":
+                    new_pace = pace
+
+                query = "UPDATE pace_for_km SET pace = %s WHERE id_training = %s AND km = %s"
+                values = (new_pace, id_training, km)
+                cursor.execute(query, values)
+                conn.commit()
+
+        conn.close()
         
-    new_heart_rate_AVG = input(f"La frecuencia cardiaca promedio actual es {heart_rate_AVG}. Ingrese la nueva frecuencia cardiaca promedio o enter para conservar la actual: ")
-    if new_heart_rate_AVG == "":
-        new_heart_rate_AVG = heart_rate_AVG
-
-    query = "SELECT * FROM cadence WHERE id_training = %s"
-    values = (id_training,)
-    cursor.execute(query, values)
-    data = cursor.fetchall()
-
-    cadence_AVG = data[0][2]
-    cadence_max = data[0][3]
-
-    new_cadence_AVG = input(f"La cadencia promedio actual es {cadence_AVG}. Ingrese la nueva cadencia promedio o enter para conservar la actual: ")
-    if new_cadence_AVG == "":
-        new_cadence_AVG = cadence_AVG
-
-    new_cadence_max = input(f"La cadencia máxima actual es {cadence_max}. Ingrese la nueva cadencia máxima o enter para conservar la actual: ")
-    if new_cadence_max == "":
-        new_cadence_max = cadence_max
-
-    query = "SELECT * FROM heart_rate WHERE id_training = %s"
-    values = (id_training,)
-    cursor.execute(query, values)
-    data = cursor.fetchall()
-
-    heart_rate_AVG = data[0][2]
-    heart_rate_max = data[0][3]
-    ligth_pace = data[0][4]
-    intensive_pace = data[0][5]
-    aerobic_pace = data[0][6]
-    anaerobic_pace = data[0][7]
-    vo2_max = data[0][8]
-
-    new_heart_rate_AVG = input(f"La frecuencia cardiaca promedio actual es {heart_rate_AVG}. Ingrese la nueva frecuencia cardiaca promedio o enter para conservar la actual: ")
-    if new_heart_rate_AVG == "":
-        new_heart_rate_AVG = heart_rate_AVG
-
-    new_heart_rate_max = input(f"La frecuencia cardiaca máxima actual es {heart_rate_max}. Ingrese la nueva frecuencia cardiaca máxima o enter para conservar la actual: ")
-    if new_heart_rate_max == "":
-        new_heart_rate_max = heart_rate_max
-        
-    new_ligth_pace = input(f"El ritmo ligero actual es {ligth_pace}. Ingrese el nuevo ritmo ligero o enter para conservar el actual: ")
-    if new_ligth_pace == "":
-        new_ligth_pace = ligth_pace
-
-    new_intensive_pace = input(f"El ritmo intenso actual es {intensive_pace}. Ingrese el nuevo ritmo intenso o enter para conservar el actual: ")
-    if new_intensive_pace == "":
-        new_intensive_pace = intensive_pace
-
-    new_aerobic_pace = input(f"El ritmo aeróbico actual es {aerobic_pace}. Ingrese el nuevo ritmo aeróbico o enter para conservar el actual: ")
-    if new_aerobic_pace == "":
-        new_aerobic_pace = aerobic_pace
-
-    new_anaerobic_pace = input(f"El ritmo anaeróbico actual es {anaerobic_pace}. Ingrese el nuevo ritmo anaeróbico o enter para conservar el actual: ")
-    if new_anaerobic_pace == "":
-        new_anaerobic_pace = anaerobic_pace
-
-    new_vo2_max = input(f"El VO2 máx actual es {vo2_max}. Ingrese el nuevo VO2 máx o enter para conservar el actual: ")
-    if new_vo2_max == "":
-        new_vo2_max = vo2_max
-
-    query = "SELECT * FROM pace WHERE id_training = %s"
-    values = (id_training,)
-    cursor.execute(query, values)
-    data = cursor.fetchall()
-
-    pace = data[0][2]
-    pace_max = data[0][3]
-
-    new_pace = input(f"El ritmo actual es {pace}. Ingrese el nuevo ritmo o enter para conservar el actual: ")
-    if new_pace == "":
-        new_pace = pace
-
-    new_pace_max = input(f"El ritmo máximo actual es {pace_max}. Ingrese el nuevo ritmo máximo o enter para conservar el actual: ")
-    if new_pace_max == "":
-        new_pace_max = pace_max
-
-    query = "SELECT * FROM stride_cm WHERE id_training = %s"
-    values = (id_training,)
-    cursor.execute(query, values)
-    data = cursor.fetchall()
-
-    stride_AVG = data[0][2]
-    stride_max = data[0][3]
-
-    new_stride_AVG = input(f"La zancada promedio actual es {stride_AVG}. Ingrese la nueva zancada promedio o enter para conservar la actual: ")
-    if new_stride_AVG == "":
-        new_stride_AVG = stride_AVG
-
-    new_stride_max = input(f"La zancada máxima actual es {stride_max}. Ingrese la nueva zancada máxima o enter para conservar la actual: ")
-    if new_stride_max == "":
-        new_stride_max = stride_max
-
-    query = "SELECT * FROM pace_for_km WHERE id_training = %s"
-    values = (id_training,)
-    cursor.execute(query, values)
-    data_km = cursor.fetchall()
-
-    query = "UPDATE health SET date = %s, calories = %s, steps = %s, distance = %s, moviment = %s, in_training = %s, id_user_update = %s, update_date = %s WHERE id = %s"
-    values = (new_date, new_calories, new_steps, new_distance, new_moviment, new_in_training, id_user_update, update_date, id_health)
-    cursor.execute(query, values)
-    conn.commit()
-
-    query = "UPDATE training SET id_type_training = %s, km_distance = %s, kcal_active = %s, kcal_total = %s, pace = %s, steps = %s, heart_rate_AVG = %s WHERE id_health = %s"
-    values = (new_id_type_training, new_km_distance, new_kcal_active, new_kcal_total, new_pace, new_steps, new_heart_rate_AVG, id_health)
-    cursor.execute(query, values)
-    conn.commit()
-
-    query = "UPDATE cadence SET cadence_AVG = %s, cadence_max = %s WHERE id_training = %s"
-    values = (new_cadence_AVG, new_cadence_max, id_training)
-    cursor.execute(query, values)
-    conn.commit()
-
-    query = "UPDATE heart_rate SET heart_rate_AVG = %s, heart_rate_max = %s, ligth_pace = %s, intensive_pace = %s, aerobic_pace = %s, anaerobic_pace = %s, vo2_max = %s WHERE id_training = %s"
-    values = (new_heart_rate_AVG, new_heart_rate_max, new_ligth_pace, new_intensive_pace, new_aerobic_pace, new_anaerobic_pace, new_vo2_max, id_training)
-    cursor.execute(query, values)
-    conn.commit()
-
-    query = "UPDATE pace SET pace = %s, pace_max = %s WHERE id_training = %s"
-    values = (new_pace, new_pace_max, id_training)
-    cursor.execute(query, values)
-    conn.commit()
-
-    query = "UPDATE stride_cm SET stride_AVG = %s, stride_max = %s WHERE id_training = %s"
-    values = (new_stride_AVG, new_stride_max, id_training)
-    cursor.execute(query, values)
-    conn.commit()
-
-    for i in data_km:
-            km = i[2]
-            pace = i[3]
-
-            new_pace = input(f"\nEl ritmo en el kilometro {km} fue {pace}. Ingrese el nuevo ritmo(HH:mm:ss) o enter para conservar el actual: ")
-            if new_pace == "":
-                new_pace = pace
-
-            query = "UPDATE pace_for_km SET pace = %s WHERE id_training = %s AND km = %s"
-            values = (new_pace, id_training, km)
-            cursor.execute(query, values)
-            conn.commit()
-
-    conn.close()
-    
-    print("Registro de fitness actualizado con éxito")
+        print("Registro de fitness actualizado con éxito")
